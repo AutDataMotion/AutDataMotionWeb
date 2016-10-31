cf355d843176af22ac9ee9c55cd023327ffcd0d3
zhangkang <191485919@qq.com>
2016-10-31 11:11:30 +0800

    update html and tree model

--- WebContent/ui/SuperAdmin/js/functions.js
+++ WebContent/ui/SuperAdmin/js/functions.js
@@ -16,46 +16,46 @@ $(document).ready(function(){
 				'<div class="modal-body">' +
 				    '<div class="row template-skins">' +
 					'<a data-skin="skin-blur-violate" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-violate.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-violate.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-lights" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-lights.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-lights.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-city" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-city.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-city.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-greenish" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-greenish.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-greenish.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-night" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-night.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-night.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-blue" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-blue.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-blue.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-sunny" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-sunny.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-sunny.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-cloth" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-cloth.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-cloth.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-tectile" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-tectile.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-tectile.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-chrome" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-chrome.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-chrome.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-ocean" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-ocean.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-ocean.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-sunset" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-sunset.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-sunset.jpg" alt="">' +
 					'</a>' +
 					'<a data-skin="skin-blur-yellow" class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-yellow.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-yellow.jpg" alt="">' +
 					'</a>' +
 					'<a  data-skin="skin-blur-kiwi"class="col-sm-2 col-xs-4" href="">' +
-					    '<img src="img/skin-kiwi.jpg" alt="">' +
+					    '<img src="/ui/SuperAdmin/img/skin-kiwi.jpg" alt="">' +
 					'</a>' +
 				    '</div>' +
 				'</div>' +
@@ -63,12 +63,11 @@ $(document).ready(function(){
 			'</div>' +
 		    '</div>';
     $('#main').prepend(settings);
-            
     $('body').on('click', '.template-skins > a', function(e){
-	e.preventDefault();
-	var skin = $(this).data('skin');
-	$('body').attr('id', skin);
-	$('#changeSkin').modal('hide');
+		e.preventDefault();
+		var skin = $(this).data('skin');
+		$('body').attr('id', skin);
+		$('#changeSkin').modal('hide');
     });
     
     /* --------------------------------------------------------
