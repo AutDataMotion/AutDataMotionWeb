54b4cf7a09fe4e3665f2af84ffcb7e3583fb5cb0
Qman29 <qinman@csu.ac.cn>
2016-11-04 15:52:21 +0800

    update download.html

--- datamotion/datamotion/mvc/t6_dwnloadfile/T6_dwnloadfileController.java
+++ datamotion/datamotion/mvc/t6_dwnloadfile/T6_dwnloadfileController.java
@@ -1,5 +1,8 @@
 package datamotion.mvc.t6_dwnloadfile;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import com.platform.constant.ConstantRender;
 import com.platform.mvc.base.BaseController;
 import com.platform.mvc.base.BaseModel;
@@ -8,7 +11,10 @@ import org.apache.log4j.Logger;
 
 import com.jfinal.aop.Before;
 import com.jfinal.aop.Clear;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 
+import csuduc.platform.util.generID.UUIDGener;
 import datamotion.constant.ConstantInitMy;
 
 
@@ -42,9 +48,55 @@ public class T6_dwnloadfileController extends BaseController {
 		/*paging(ConstantInitMy.db_dataSource_main, splitPage, BaseModel.sqlId_splitPage_select, T6_dwnloadfile.sqlId_splitPage_from);
 		renderWithPath(pthv+"list.html");*/
 		
+		List<Record> res = Db.use(ConstantInitMy.db_dataSource_main)
+				.find("select * from t6_dwnloadfile");//id, key_, namesrc, timedo, pathsrc, pathdest, filesize, status_
+		setAttr("resInit", res);
+		System.out.println("res============"+res.toString());
 		renderWithPath(pthv+"dwnloadfile.html");
 	}
 	
+	@Clear
+	public void add(){
+		String pathsrc="D://MWI//";
+		String namesrc="TT_TT02_MWI_VNI_IMG_20161102000000_20161102000000_20161102000000_000_0C.csv";
+		String[] namesStrings = namesrc.split("_");
+		String pathdest="E://MWI//";
+//		String timedo=getPara("datatype");
+		int filesize= 3333;
+		String station=namesStrings[0];
+		String aircraft=namesStrings[1];
+		String sensor=namesStrings[2];
+		String datatype=namesStrings[3];
+		String datalevel=namesStrings[9];
+		String camera=namesStrings[4];
+		Timestamp timerecive=new Timestamp(System.currentTimeMillis());
+//		String timecollectstart=getPara("patharchive");
+//		String timecollectend=getPara("ischeckout");
+		String suffix=".csv";
+		int status_=1;
+//		String timeadd=getPara("namemdldes");
+		Record t6_dwnloadfileRecord=new Record()
+		.set("key_", UUIDGener.getUUIDShort())
+		.set("pathsrc", pathsrc)
+		.set("namesrc", namesrc)
+		.set("pathdest", pathdest)
+		.set("timedo", new Timestamp(System.currentTimeMillis()))
+		.set("filesize", filesize)
+		.set("station", station)
+		.set("aircraft", aircraft)
+		.set("sensor", sensor)
+		.set("datatype", datatype)
+		.set("datalevel", datalevel)
+		.set("camera", camera)
+		.set("timerecive", timerecive)
+		.set("timecollectstart", new Timestamp(System.currentTimeMillis()))
+		.set("timecollectend", new Timestamp(System.currentTimeMillis()))
+		.set("suffix", suffix)
+		.set("status_", status_)
+		.set("timeadd", new Timestamp(System.currentTimeMillis()));
+		Db.use(ConstantInitMy.db_dataSource_main)
+		  .save("t6_dwnloadfile", t6_dwnloadfileRecord);
+	}
 	@Clear
 	public void viewControlPanel() {
 		renderWithPath(pthvf+"controlpanel.html");
